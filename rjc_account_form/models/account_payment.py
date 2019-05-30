15b82c5754cb46012b56ad3a3b8d6bd7f3d192a9
Saran L <saranp502@hotmail.com>
2019-05-30 09:23:50 +0700

    [DEL] function hide print menu

--- rjc_account_form/models/account_payment.py
+++ rjc_account_form/models/account_payment.py
@@ -25,28 +25,3 @@ class account_payment(models.Model):
                 [('move_line_id', '=', payment.id)])
             if payment_intransit:
                 return payment_intransit
-
-    @api.multi
-    def remove_menu_print(self, res, reports):
-        # Remove reports menu
-        for report in reports:
-            reports = self.env.ref(report, raise_if_not_found=False)
-            for rec in res.get('toolbar', {}).get('print', []):
-                if rec.get('id', False) in reports.ids:
-                    del res['toolbar']['print'][
-                        res.get('toolbar', {}).get('print').index(rec)]
-        return res
-
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form',
-                        toolbar=False, submenu=False):
-        hide_reports_base = [
-            'account.action_report_payment_receipt',
-        ]
-        res = super(account_payment, self).fields_view_get(
-            view_id=view_id, view_type=view_type,
-            toolbar=toolbar, submenu=submenu)
-        if res and view_type in ['tree', 'form']:
-            # del menu report customer and vendor
-            self.remove_menu_print(res, hide_reports_base)
-        return res
--- rjc_purchase_order_form/models/purchase.py
+++ rjc_purchase_order_form/models/purchase.py
@@ -21,29 +21,3 @@ class PurchaseOrder(models.Model):
         for rec in self.order_line:
             amount_total += rec.product_qty * rec.price_unit
         return amount_total
-
-    @api.multi
-    def remove_menu_print(self, res, reports):
-        # Remove reports menu
-        for report in reports:
-            reports = self.env.ref(report, raise_if_not_found=False)
-            for rec in res.get('toolbar', {}).get('print', []):
-                if rec.get('id', False) in reports.ids:
-                    del res['toolbar']['print'][
-                        res.get('toolbar', {}).get('print').index(rec)]
-        return res
-
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form',
-                        toolbar=False, submenu=False):
-        hide_reports_purchase = [
-            'purchase.action_report_purchase_order',
-            'purchase.report_purchase_quotation',
-        ]
-        res = super(PurchaseOrder, self).fields_view_get(
-            view_id=view_id, view_type=view_type,
-            toolbar=toolbar, submenu=submenu)
-        if res and view_type in ['tree', 'form']:
-            # del menu report customer and vendor
-            self.remove_menu_print(res, hide_reports_purchase)
-        return res
