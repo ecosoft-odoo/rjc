af3201ff560dd22035822225c97aa1a6c0f82d99
Saran <saranp502@hotmail.com>
2019-06-11 11:54:24 +0700

    [12.0][ADD] rjc_account_asset and fix l10n_th_vendor_tax_invoice

---
+++ rjc_account_asset/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'RJC Account Asset',
+    'summary': 'Modify field in account asset module',
+    'version': '12.0.1.0.0',
+    'category': 'Account',
+    'website': 'https://github.com/ecosoft-odoo/rjc',
+    'author': 'Ecosoft',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'account_asset_management',
+    ],
+    'data': [
+        'views/account_asset_profile.xml',
+        'views/account_asset.xml',
+    ],
+}
