50519ba6dd9b2aa8fa5ceb57db76c2399cb57707
Saran <saranp502@hotmail.com>
2019-06-11 13:57:09 +0700

    [FIX] Add field on the payment

--- rjc_account/models/account_payment.py
+++ rjc_account/models/account_payment.py
@@ -6,15 +6,35 @@ from odoo import fields, models, api
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
+    value_date = fields.Date(
+        required=True,
+        states={'draft': [('readonly', False)]},
+        readonly=True,
+    )
     payment_ref = fields.Char(
         states={'draft': [('readonly', False)]},
         readonly=True,
         )
+    cheque_no = fields.Char(
+        string='Cheque No.',
+        states={'draft': [('readonly', False)]},
+        readonly=True,
+    )
+    validate_by = fields.Many2one(
+        comodel_name='res.users',
+        readonly=True,
+    )
     notes = fields.Text(
         string='Internal Notes',
         states={'draft': [('readonly', False)]},
         readonly=True,
         )
+
+    @api.multi
+    def post(self):
+        for payment in self:
+            payment.validate_by = self.env.user.id
+        return super(AccountPayment, self).post()
 
 
 class AccountRegisterPayments(models.TransientModel):
@@ -24,10 +44,18 @@ class AccountRegisterPayments(models.TransientModel):
     notes = fields.Text(
         string='Internal Notes',
     )
+    value_date = fields.Date(
+        required=True,
+    )
+    cheque_no = fields.Char(
+        string='Cheque No.',
+    )
 
     @api.multi
     def _prepare_payment_vals(self, invoices):
         values = super()._prepare_payment_vals(invoices)
         values['payment_ref'] = self.payment_ref
         values['notes'] = self.notes
+        values['cheque_no'] = self.cheque_no
+        values['value_date'] = self.value_date
         return values
--- rjc_account/readme/DESCRIPTION.rst
+++ rjc_account/readme/DESCRIPTION.rst
@@ -1,3 +1,3 @@
 This module include
 * Add field Preprint Number on account invoice view
-* Add field Payment Ref, Internal Notes on payment view.
+* Add field Payment Ref, Internal Notes, Validate By, Value Date and Cheque No. on payment view.
