a0b026f277cc8a012b9163479dd2cf4d3728913d
Saran <saranp502@hotmail.com>
2019-06-13 15:56:25 +0700

    [FIX] account_payment_intransit_reversal

--- account_payment_intransit_reversal/tests/test_payment_intransit_reversal.py
+++ account_payment_intransit_reversal/tests/test_payment_intransit_reversal.py
@@ -1,50 +1,157 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 from odoo.tests.common import SavepointCase, Form
+import time
 
 
 class TestPaymentIntransitReversal(SavepointCase):
 
     @classmethod
-    def setUpClass(cls):
-        super(TestPaymentIntransitReversal, cls).setUpClass()
-        cls.partner = cls.env['res.partner'].create({'name': 'Test'})
-        cls.account_type_receivable = cls.env['account.account.type'].create({
-            'name': 'Test Receivable',
-            'type': 'receivable',
+    def setUpClass(self):
+        super().setUpClass()
+        self.invoice_model = self.env['account.invoice']
+        self.register_payments_model = self.env['account.register.payments']
+        self.payment_model = self.env['account.payment']
+        self.journal_model = self.env['account.journal']
+        self.account_account_model = self.env['account.account']
+        self.invoice_line_model = self.env['account.invoice.line']
+        self.move_line_model = self.env['account.move.line']
+        self.res_partner_bank_model = self.env['res.partner.bank']
+        self.payment_intransit_model = self.env['account.payment.intransit']
+        self.payment_intransit_line_model = \
+            self.env['account.payment.intransit.line']
+
+        self.partner_agrolait = self.env.ref('base.res_partner_2')
+        self.currency_eur_id = self.env.ref('base.EUR').id
+        self.currency_usd_id = self.env.ref('base.USD').id
+        self.main_company = self.env.ref('base.main_company')
+        self.product = self.env.ref('product.product_product_4')
+        self.payment_method_manual_in = self.env.ref(
+            'account.account_payment_method_manual_in')
+        self.main_company.payment_intransit_post_move = True
+
+        self.env.cr.execute(
+            """UPDATE res_company SET currency_id = %s
+            WHERE id = %s""",
+            (self.main_company.id, self.currency_eur_id),
+        )
+        self.account_receivable = self.account_account_model.search(
+            [('code', '=', '411100')], limit=1)
+        if not self.account_receivable:
+            self.account_receivable = self.account_account_model.create(
+                {'code': '411100',
+                 'name': 'Debtors - (test)',
+                 'reconcile': True,
+                 'user_type_id': 1
+                 })
+
+        self.account_type_revenue = self.env.ref(
+            'account.data_account_type_revenue').id
+        self.account_type_receivable = self.env.ref(
+            'account.data_account_type_receivable').id
+        self.account_receivable = self.account_account_model.search([
+            ('user_type_id', '=', self.account_type_receivable)], limit=1)
+        self.account_revenue = self.account_account_model.search([
+            ('user_type_id', '=', self.account_type_revenue)], limit=1)
+
+        # Create journal Bank Intransit
+        self.intransit_account_id = self.account_account_model.search(
+            [('code', '=', '111')], limit=1)
+        if not self.intransit_account_id:
+            self.intransit_account_id = self.account_account_model.create(
+                {'code': '111',
+                 'name': 'Intransit',
+                 'reconcile': True,
+                 'user_type_id': self.account_type_receivable
+                 })
+        self.journal_account = self.journal_model.search([
+            ('code', '=', 'BIT')], limit=1)
+        if not self.journal_account:
+            self.journal_account = self.journal_model.create({
+                'name': 'Bank Intransit',
+                'type': 'bank',
+                'code': 'BIT'})
+        self.journal_account.default_debit_account_id = \
+            self.intransit_account_id
+        self.journal_account.default_credit_account_id = \
+            self.intransit_account_id
+
+        # Create Bank Account
+        self.bank_account_id = self.account_account_model.search(
+            [('code', '=', '222')], limit=1)
+        if not self.bank_account_id:
+            self.bank_account_id = self.account_account_model.create(
+                {'code': '222',
+                 'name': 'SCB',
+                 'user_type_id': 3
+                 })
+        self.partner_bank_id = self.res_partner_bank_model.search(
+            [('partner_id', '=', self.main_company.partner_id.id)], limit=1)
+        if not self.partner_bank_id:
+            self.partner_bank_id = self.res_partner_bank_model.create(
+                {'acc_number': 'SCB 222 222 222',
+                 'partner_id': self.main_company.partner_id.id,
+                 })
+
+        self.bank_journal = self.journal_model.search([
+            ('code', '=', 'BNK1')], limit=1)
+        if not self.bank_journal:
+            self.bank_journal = self.journal_model.create({
+                'name': 'SCB',
+                'type': 'bank',
+                'code': 'BNK1'})
+        self.bank_journal.default_debit_account_id = self.bank_account_id
+        self.bank_journal.default_credit_account_id = self.bank_account_id
+        self.bank_journal.update_posted = True
+
+    def create_invoice(self, amount=100, type='out_invoice', currency_id=None):
+        """ Returns an open invoice """
+        invoice = self.invoice_model.create({
+            'partner_id': self.partner_agrolait.id,
+            'currency_id': currency_id,
+            'name': type == 'out_invoice' and
+            'invoice to client' or 'invoice to supplier',
+            'account_id': self.account_receivable.id,
+            'type': type,
+            'date_invoice': time.strftime('%Y-%m-%d'),
         })
-        cls.account_type_regular = cls.env['account.account.type'].create({
-            'name': 'Test Regular',
-            'type': 'other',
-        })
-        cls.account_receivable = cls.env['account.account'].create({
-            'name': 'Test Receivable',
-            'code': 'TEST_AR',
-            'user_type_id': cls.account_type_receivable.id,
-            'reconcile': True,
-        })
-        cls.account_income = cls.env['account.account'].create({
-            'name': 'Test Income',
-            'code': 'TEST_IN',
-            'user_type_id': cls.account_type_regular.id,
-            'reconcile': False,
-        })
-        cls.sale_journal = cls.env['account.journal'].\
-            search([('type', '=', 'sale')])[0]
-        cls.invoice = cls.env['account.invoice'].create({
-            'name': "Test Customer Invoice",
-            'journal_id': cls.sale_journal.id,
-            'partner_id': cls.partner.id,
-            'account_id': cls.account_receivable.id,
-        })
-        cls.invoice_line = cls.env['account.invoice.line']
-        cls.invoice_line1 = cls.invoice_line.create({
-            'invoice_id': cls.invoice.id,
-            'name': 'Line 1',
-            'price_unit': 200.0,
-            'account_id': cls.account_income.id,
+        self.invoice_line_model.create({
+            'product_id': self.product.id,
             'quantity': 1,
+            'price_unit': amount,
+            'invoice_id': invoice.id,
+            'name': 'something',
+            'account_id': self.account_revenue.id,
         })
+        invoice.action_invoice_open()
+        return invoice
+
+    def create_payment_intransit(
+            self, move_lines, allocation=None, curr_id=None):
+        """ Returns an validated payment intransit """
+        payment_intransit = self.payment_intransit_model.create({
+            'partner_id': self.partner_agrolait.id,
+            'journal_id': self.journal_account.id,
+            'bank_journal_id': self.bank_journal.id,
+            'receipt_date': time.strftime('%Y-%m-%d'),
+            'currency_id': curr_id or self.main_company.currency_id.id,
+        })
+        for move_line in move_lines:
+            self.payment_intransit_line_model.create({
+                'move_line_id': move_line.id,
+                'receipt_type': 'cash',
+                'allocation': allocation or 10,
+                'intransit_id': payment_intransit.id
+            })
+        return payment_intransit
+
+    def create_payment(self, ctx):
+        register_payments = self.register_payments_model.with_context(
+            ctx).create({
+                'payment_date': time.strftime('%Y-%m-%d'),
+                'journal_id': self.journal_account.id,
+                'payment_method_id': self.payment_method_manual_in.id})
+        register_payments.create_payments()
 
     def test_journal_invoice_cancel_reversal(self):
         """ Tests cancel with reversal, end result must follow,
@@ -52,27 +159,38 @@ class TestPaymentIntransitReversal(SavepointCase):
         - Status is changed to cancel
         """
         # Test journal
-        self.assertFalse(self.sale_journal.is_cancel_reversal)
-        self.sale_journal.write({'update_posted': True,
-                                 'cancel_method': 'reversal'})
-        # Open invoice
-        self.invoice.action_invoice_open()
-        move = self.invoice.move_id
+        self.journal_account.write({'update_posted': True,
+                                    'cancel_method': 'reversal'})
+        self.assertTrue(self.journal_account.is_cancel_reversal)
+        # Create and Open invoice
+        inv_1 = self.create_invoice(
+            amount=100, currency_id=self.currency_eur_id)
+        inv_2 = self.create_invoice(
+            amount=200, currency_id=self.currency_eur_id)
+        ctx = {'active_model': 'account.invoice',
+               'active_ids': [inv_1.id, inv_2.id]}
+        # Create Payment
+        self.create_payment(ctx)
+        # Create Payment Intransit
+        check_aml = self.move_line_model.search([]).filtered(
+            lambda l: l.reconciled is False and l.debit > 0 and
+            l.account_id == self.intransit_account_id)
+        payment_intransit = self.create_payment_intransit(check_aml)
+        try:
+            payment_intransit.validate_payment_intransit()
+        except Exception:
+            # delete payment difference
+            total_amount = \
+                sum(payment_intransit.receipt_line_ids.mapped('allocation'))
+            payment_intransit.write({'manual_total_amount': total_amount})
+            payment_intransit.validate_payment_intransit()
         # Click Cancel will open reverse document wizard
-        res = self.invoice.action_invoice_cancel()
+        res = payment_intransit.action_intransit_cancel()
         self.assertEqual(res['res_model'], 'reverse.account.document')
         # Cancel invoice
-        ctx = {'active_model': 'account.invoice',
-               'active_ids': [self.invoice.id]}
+        ctx = {'active_model': 'account.payment.intransit',
+               'active_ids': [payment_intransit.id]}
         f = Form(self.env[res['res_model']].with_context(ctx))
         cancel_wizard = f.save()
         cancel_wizard.action_cancel()
-        reversed_move = move.reverse_entry_id
-        move_reconcile = move.mapped('line_ids').mapped('full_reconcile_id')
-        reversed_move_reconcile = \
-            reversed_move.mapped('line_ids').mapped('full_reconcile_id')
-        # Check
-        self.assertTrue(move_reconcile)
-        self.assertTrue(reversed_move_reconcile)
-        self.assertEqual(move_reconcile, reversed_move_reconcile)
-        self.assertEqual(self.invoice.state, 'cancel')
+        self.assertEqual(payment_intransit.state, 'cancel')
