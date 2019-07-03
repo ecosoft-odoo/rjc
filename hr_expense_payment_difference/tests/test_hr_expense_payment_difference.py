6610d5ee202ac253ef3fb8626156fdefa76b9a14
Saran <saranp502@hotmail.com>
2019-07-03 16:23:55 +0700

    [FIX] add test script hr_expense_payment_diff

--- hr_expense_payment_difference/tests/test_hr_expense_payment_difference.py
+++ hr_expense_payment_difference/tests/test_hr_expense_payment_difference.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 from odoo.tests.common import SavepointCase
-from odoo.exceptions import ValidationError
+from odoo.tests.common import Form
 
 
 class TestHrExpensePaymentDifference(SavepointCase):
@@ -12,14 +12,19 @@ class TestHrExpensePaymentDifference(SavepointCase):
         super().setUpClass()
         self.expense_model = self.env['hr.expense']
         self.expense_sheet_model = self.env['hr.expense.sheet']
-        # self.register_payments_model = self.env['account.register.payments']
 
         self.product = self.env.ref("product.product_product_4")
+        self.journal_bank = self.env['account.journal'].search(
+            [('type', '=', 'bank')], limit=1)
+        self.account_id = self.env['account.account'].search([], limit=1)
 
+        employee_home = self.env['res.partner'].create({
+            'name': 'Employee Home Address',
+        })
         self.employee_1 = self.env['hr.employee'].create({
             'name': 'Employee 1',
+            'address_home_id': employee_home.id,
         })
-
         self.expense_1 = self.create_expense(self, 'Expense Test1')
         self.expense_2 = self.create_expense(self, 'Expense Test2')
         self.sheet = self.expense_sheet_model.create({
@@ -39,77 +44,25 @@ class TestHrExpensePaymentDifference(SavepointCase):
         expense.action_submit_expenses()
         return expense
 
-    # def create_payment(self, ctx):
-    #     register_payments = \
-    #         self.register_payments_model.with_context(ctx).create({
-    #             'journal_id': self.journal_bank.id,
-    #             'payment_method_id': self.payment_method_manual_in.id
-    #         })
-    #     return register_payments.create_payments()
-
     def test_1_create_payment_expense(self):
         self.sheet.expense_line_ids = [
             (6, 0, [self.expense_1.id])]
-        print("=====11111==========")
         self.sheet.action_submit_sheet()
-        print("=====2222==========")
         self.sheet.approve_expense_sheets()
-        print(self.sheet.state)
-        print("=====333==========")
         self.sheet.action_sheet_move_create()
-        print("=====444==========")
-        print(self.sheet.state)
-        print("===============")
-        x=1/0
+        self.assertEqual(self.sheet.total_amount, self.product.standard_price)
 
-    # def test_2_invoice_currency(self):
-    #     ctx1 = {'active_model': 'account.invoice',
-    #             'active_ids': [self.inv_1.id, self.inv_3.id],
-    #             'bill_type': 'out_invoice'}
-    #     with self.assertRaises(ValidationError):
-    #         self.billing_model.with_context(ctx1).create({})
-    #     # create billing directly
-    #     self.billing_model.create({'partner_id': self.partner_agrolait.id})
-    #
-    # def test_3_validate_billing_state_not_open(self):
-    #     ctx = {'active_model': 'account.invoice',
-    #            'active_ids': [self.inv_1.id]}
-    #     self.create_payment(ctx)
-    #     customer_billing = self.billing_model.with_context(ctx).create({})
-    #     with self.assertRaises(ValidationError):
-    #         customer_billing.validate_billing()
-    #
-    # def test_4_billing_fields_view_get(self):
-    #     ctx = {'active_model': 'account.invoice',
-    #            'active_ids': [self.inv_1.id]}
-    #     customer_billing = self.billing_model.with_context(ctx).create({})
-    #     # check invoice is billed
-    #     with self.assertRaises(ValidationError):
-    #         customer_billing.fields_view_get()
-    #
-    #     self.create_payment(ctx)
-    #     # check invoice is state paid
-    #     customer_billing = self.billing_model.with_context(ctx).create({})
-    #     with self.assertRaises(ValidationError):
-    #         customer_billing.fields_view_get()
-    #
-    # def test_5_create_billing_from_selected_invoices(self):
-    #     """ Create two invoices, post it and send context to Billing """
-    #     ctx = {'active_model': 'account.invoice',
-    #            'active_ids': [self.inv_1.id, self.inv_2.id],
-    #            'bill_type': 'out_invoice'}
-    #     customer_billing1 = self.billing_model.with_context(ctx).create({})
-    #     customer_billing1._onchange_bill_type()
-    #     self.assertEqual(customer_billing1.state, 'draft')
-    #     customer_billing1.validate_billing()
-    #     self.assertEqual(customer_billing1.state, 'billed')
-    #     self.assertEqual(customer_billing1.invoice_related_count, 2)
-    #     customer_billing1.invoice_relate_billing_tree_view()
-    #     customer_billing1.action_cancel()
-    #     customer_billing1.action_cancel_draft()
-    #
-    #     customer_billing2 = self.billing_model.with_context(ctx).create({})
-    #     customer_billing2.validate_billing()
-    #     self.create_payment(ctx)
-    #     with self.assertRaises(ValidationError):
-    #         customer_billing2.action_cancel()
+        # Register Payment
+        ctx = {'active_model': 'hr.expense.sheet',
+               'active_ids': [self.sheet.id],
+               'active_id': self.sheet.id,
+               'default_amount': self.product.standard_price}
+        PaymentWizard = self.env['hr.expense.sheet.register.payment.wizard']
+        with Form(PaymentWizard.with_context(ctx)) as f:
+            f.journal_id = self.journal_bank
+            f.amount = self.product.standard_price - 20.0
+            f.payment_difference_handling = 'reconcile'
+            f.writeoff_account_id = self.account_id
+        payment_wizard = f.save()
+        self.assertEqual(payment_wizard.payment_difference, 20.0)
+        payment_wizard.expense_post_payment()
