a234f6e51a05e1c585c713d9f50f3db89d93be6d
Saran <saranp502@hotmail.com>
2019-07-18 09:52:09 +0700

    [12.0][ADD] rjc_sale_form

---
+++ rjc_sale_form/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'RJC Sale Form',
+    'version': '12.0.1.0.0',
+    'author': 'Ecosoft',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/ecosoft-odoo/rjc',
+    'category': 'Report',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'reports/sale_report_templates.xml',
+    ],
+    'installable': True,
+}
