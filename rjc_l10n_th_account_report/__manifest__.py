0ab7efd492dafda57da6797241e8d44469d0e3f2
Saran <saranp502@hotmail.com>
2019-08-27 15:22:33 +0700

    [12.0][ADD] rjc_l10n_th_account_report

---
+++ rjc_l10n_th_account_report/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'RJC Accounting Reports',
+    'version': '12.0.1.0.0',
+    'author': 'Ecosoft, Odoo Community Association (OCA)',
+    'website': 'https://github.com/ecosoft-odoo/rjc',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'depends': [
+        'l10n_th_account_report'
+    ],
+    'data': [],
+    'installable': True,
+    'development_status': 'alpha',
+}
