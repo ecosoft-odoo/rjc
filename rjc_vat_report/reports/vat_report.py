de3ce48c88dfdf2202d2fefeeae4a57eaf804174
Saran <saranp502@hotmail.com>
2019-08-29 17:45:06 +0700

    [FIX] query vat report

--- rjc_vat_report/reports/vat_report.py
+++ rjc_vat_report/reports/vat_report.py
@@ -27,14 +27,17 @@ class VatReport(models.TransientModel):
         self._cr.execute("""
             SELECT aml.id as id, am.company_id, am.name, aml.account_id,
                 aml.tax_invoice, aml.partner_id, aml.date, aml.tax_date,
-                aml.tax_base_amount as base_amount,
+                CASE WHEN ai.type = 'out_refund'
+                then -aml.tax_base_amount
+                else aml.tax_base_amount end as base_amount,
                 CASE WHEN aa.internal_group = 'asset'
                 then aml.balance else -aml.balance end as tax_amount
             FROM account_move_line aml
             JOIN account_move am on aml.move_id = am.id
             JOIN account_account aa on aa.id = aml.account_id
-            WHERE tax_line_id is not null and aml.date >= %s and aml.date <= %s
-                and aml.company_id = %s and aml.account_id = %s
+            LEFT JOIN account_invoice ai on ai.id = aml.invoice_id
+            WHERE aml.tax_line_id is not null and aml.date >= %s and
+                aml.date <= %s and aml.company_id = %s and aml.account_id = %s
             ORDER BY aml.tax_date
         """, (self.date_from, self.date_to, self.company_id.id,
               self.account_id.id))
