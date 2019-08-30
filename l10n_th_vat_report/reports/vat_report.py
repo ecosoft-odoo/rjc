22b5d9133d43fdf15cf61d674e972af6cc890a75
Saran <saranp502@hotmail.com>
2019-08-30 18:12:15 +0700

    [UPD] vat_report

--- l10n_th_vat_report/reports/vat_report.py
+++ l10n_th_vat_report/reports/vat_report.py
@@ -1,5 +1,6 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
 from odoo import api, fields, models
 
 
@@ -9,18 +10,15 @@ class VatReportView(models.TransientModel):
     _inherit = 'account.move.line'
     _order = 'id'
 
-    base_amount = fields.Monetary(
-        currency_field='currency_id',
-    )
-    tax_amount = fields.Monetary(
-        currency_field='currency_id',
-    )
+    base_amount = fields.Float()
+    tax_amount = fields.Float()
     tax_date = fields.Char()
 
 
 class VatReport(models.TransientModel):
     _name = 'report.vat.report'
     _description = 'Report Vat Report'
+
     # Filters fields, used for data computation
     company_id = fields.Many2one(
         comodel_name='res.company',
@@ -36,6 +34,7 @@ class VatReport(models.TransientModel):
     )
     date_from = fields.Date()
     date_to = fields.Date()
+
     # Data fields, used to browse report data
     results = fields.Many2many(
         comodel_name='vat.report.view',
@@ -49,12 +48,19 @@ class VatReport(models.TransientModel):
         self._cr.execute("""
             SELECT aml.id as id, am.company_id, am.name, aml.account_id,
                 aml.tax_invoice, aml.partner_id, aml.date, aml.tax_date,
-                aml.tax_base_amount as base_amount, aml.balance tax_amount
+                CASE WHEN ai.type in ('out_refund', 'in_refund')
+                then -aml.tax_base_amount
+                else aml.tax_base_amount end as base_amount,
+                CASE WHEN aa.internal_group = 'asset'
+                then aml.balance else -aml.balance end as tax_amount
             FROM account_move_line aml
             JOIN account_move am on aml.move_id = am.id
-            WHERE tax_line_id is not null and aml.date >= %s and aml.date <= %s
-                and aml.company_id = %s and aml.account_id = %s
-            ORDER BY aml.tax_date
+            JOIN account_account aa on aa.id = aml.account_id
+            LEFT JOIN account_invoice ai on ai.id = aml.invoice_id
+            WHERE aml.tax_line_id is not null and aml.date >= %s and
+                aml.date <= %s and aml.company_id = %s and
+                aml.account_id = %s and am.state = 'posted'
+            ORDER BY aml.tax_date, am.name
         """, (self.date_from, self.date_to, self.company_id.id,
               self.account_id.id))
         vat_report_results = self._cr.dictfetchall()
