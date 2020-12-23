3b9ea8f9a740066d31e4695c124c316d428a3ece
Kitti U <kittiu@gmail.com>
2020-12-23 16:00:10 +0700

    Update mrp_production_auto_post_inventory for Cron Job

--- mrp_production_show_post_inventory/__manifest__.py
+++ mrp_production_show_post_inventory/__manifest__.py
@@ -1,12 +1,11 @@
-# Copyright 2016-19 Eficent Business and IT Consulting Services S.L.
-# Copyright 2019 Lorenzo Battistini - TAKOBI
+# Copyright 2020 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Production Auto Post-Inventory',
-    'version': '12.0.1.0.0',
+    'name': 'Production Show Post Inventory',
+    'version': '12.0.1.0.1',
     'category': 'MRP',
-    'author': 'Eficent, '
+    'author': 'Camptocamp, '
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/oca/manufacture',
     'license': 'AGPL-3',
@@ -14,8 +13,7 @@
         'mrp',
     ],
     'data': [
-        'data/cron.xml',
-        'views/res_config_settings.xml',
+        'views/mrp_production.xml',
     ],
     'installable': True,
 }
